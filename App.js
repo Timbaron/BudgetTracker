e0a33c9e57058694c4900604cc793818bbefb7a4
timbaron <timothy.akiode@student.aul.edu.ng>
2022-09-02 10:32:12 +0100

    add home header

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Home from './components/home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#F8F0E3',
+    width: '100%',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  }
+})
---
+++ components/header.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function Header() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.info}>
+                    <Image
+                        source={require('../assets/images/profile.jpg')}
+                        style={styles.profileImage}
+                    />
+                    <View style={styles.headerText}>
+                        <Text style={styles.welcome}>Welcome</Text>
+                        <Text style={styles.name}>John Doe</Text>
+                    </View>
+                </View>
+                <View style={styles.headersettings}>
+                    <Icon name="cog" size={30} color="grey" />
+                </View>
+            </View>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F8F0E3',
+    },
+    header: {
+        width: '100%',
+        height: 100,
+        backgroundColor: 'inherit',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+    },
+    profileImage: {
+        width: 60,
+        height: 60,
+        borderRadius: 50,
+        position: 'relative',
+        top: 30,
+        left: 10,
+    },
+    info: {
+        flexDirection: 'row',
+        backgroundColor: '#F8F0E3',
+        width: '70%',
+        height: 100,
+    },
+    headerText: {
+        position: 'relative',
+        top: 40,
+        left: 20,
+    },
+    name: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    welcome: {
+        fontSize: 15,
+        color: 'grey',
+        fontWeight: 'bold',
+    },
+    headersettings: {
+        position: 'relative',
+        top: 40,
+        right: 10,
+        padding: 3,
+        borderRadius: 9,
+        height: 40,
+        width: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+    }
+});
+
