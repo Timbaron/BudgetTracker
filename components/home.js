25da4372e184901bbc49142ba20fca8a5f234611
timbaron <timothy.akiode@student.aul.edu.ng>
2022-09-02 12:13:50 +0100

    Add transaction text

--- components/home.js
+++ components/home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
 import Account from './account';
 import Header from './header';
 
@@ -7,7 +8,31 @@ export default function Home() {
         <>
             <Header />
             <Account />
+            <View style={styles.transactionText}>
+                <Text style={styles.transaction}>Transactions</Text>
+                <Text style={styles.viewAll}>View All</Text>
+            </View>
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    transactionText: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '90%',
+        marginLeft: '5%',
+        marginTop: '6%',
+    },
+    transaction: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#171717',
+    },
+    viewAll: {
+        fontSize: 15,
+        color: 'grey',
+    }
+})
 
